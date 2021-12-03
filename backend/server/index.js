c186d76d2f9980e132af8d3ef97201cab615a3e3
Matthew Olsen <mbolsen@gmail.com>
2021-12-03 09:29:20 -0700

    server up and running

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,1 +1,14 @@
+const { application } = require('express')
+const express = require('express')
+const path = require('path');
 
+
+const app = express()
+
+app.use('/', express.static(path.join(__dirname, '..', '..', 'frontend', 'dist')));
+const port = 3000;
+
+app.listen(port, () => {
+  console.log('\n------------------------------\nserver running\n------------------------------')
+});
+  
